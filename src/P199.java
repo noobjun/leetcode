bec8a96cfc6fcd6e55aea1c3489670f081a2f5b4
Jun Zhou <jun@Juns-MacBook-Pro.local>
2015-12-23 18:53:25 -0500

    I can see the tree all from way from Russia, is it a X-mas tree?

---
+++ src/P199.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by jun on 12/23/15.
+ * 199. Binary Tree Right Side View My Submissions Question
+ */
+public class P199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+
+        if (root == null)
+            return result;
+
+        List<TreeNode> prev = new ArrayList<TreeNode>();
+        List<TreeNode> next = new ArrayList<TreeNode>();
+
+        prev.add(root);
+
+        while (!prev.isEmpty()) {
+            result.add(prev.get(prev.size() - 1).val);
+
+            for (TreeNode node : prev) {
+                if (node.left != null)
+                    next.add(node.left);
+                if (node.right != null)
+                    next.add(node.right);
+            }
+
+            prev = next;
+            next = new ArrayList<TreeNode>();
+        }
+
+        return result;
+    }
+}
