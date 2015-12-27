e53afd4620ed8077041dee8893a454ab67e6a5c1
Jun Zhou <jun@Juns-MacBook-Pro.local>
2015-12-26 23:19:14 -0500

    Some comments about P220

--- src/P220.java
+++ src/P220.java
@@ -15,6 +15,12 @@ public class P220 {
                     set.ceiling(num - t) != null && set.ceiling(num - t) <= num)
                 return true;
 
+            /**
+             * Condition can also be written as, but it won't pass the boundary test case on Integer.MIN_VALUE due to overflow
+             * set.floor(num) != null && num - set.floor(num) <= t ||
+             * set.ceiling(num) != null && set.ceiling(num) - num <= t
+             */
+
             set.add(num);
 
             if (i >= k)
