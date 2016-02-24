d51710b02cadabed7ac4d55593149c811ed819c4
Jun Zhou <jun@Juns-MacBook-Pro.local>
2016-02-23 21:24:28 -0800

    Tricky linked list problem, strategy is to declare first and second node!

--- src/P24.java
+++ src/P24.java
@@ -4,6 +4,28 @@
  */
 public class P24 {
     public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
 
+        ListNode prev = new ListNode(-1);
+        prev.next = head;
+        ListNode result = prev;
+        ListNode newHead;
+
+        while (head != null && head.next != null) {
+            ListNode first = head;
+            ListNode second = head.next;
+
+            newHead = second.next;
+            second.next = first;
+
+            prev.next = second;
+            first.next = newHead; // this is needed because if the while loop terminate this cycle, then first.next would point to second, creating infinite loop
+            prev = first;
+
+            head = newHead;
+        }
+
+        return result.next;
     }
 }
