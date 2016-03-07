2453214e653fb94c6c37b608f05ca1981ae381b3
Jun Zhou <jun@Juns-MacBook-Pro.local>
2016-03-07 09:33:47 -0800

    I wanna ride some hot air balloons!

--- src/P312.java
+++ src/P312.java
@@ -2,26 +2,27 @@
  * Created by jun on 3/6/16.
  * 312. Burst Balloons
  * <p>
- * we maintain array dp[i][j], which means the max coin by popping all the balloons from i and j (inclusive)
+ * We maintain array dp[i][j], which means the max coin by popping all the balloons from i and j (inclusive)
+ * So how to find dp[i][j]? Intuitively, we need to find out the max coins we get by popping each position LAST.
+ * Let's define k where i<=k<=j. To pop position k last, it means we are doing arr[k]*arr[i-1]*arr[j+1] + dp[i][k-1] + dp[k+1][j]
+ * After we have the dp formula, it's easy, we just loop from i to j for each k and do a max(k1, k2, ... kn).
  */
 public class P312 {
-
-    public static void main(String[] args) {
-        System.out.println(String.valueOf(2.0/3.0));
-    }
     public int maxCoins(int[] nums) {
         int[] arr = new int[nums.length + 2];
         arr[0] = 1;
-        arr[nums.length] = 1;
+        arr[arr.length - 1] = 1;
         System.arraycopy(nums, 0, arr, 1, nums.length);
+        int[][] dp = new int[arr.length][arr.length]; // default value is 0
 
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 1; j + i - 1<= nums.length; j++) {
-
-
+        for (int len = 1; len <= nums.length; len++) {
+            for (int left = 1; left + len - 1 <= nums.length; left++) {
+                int right = left + len - 1;
+                for (int k = left; k <= right; k++) {
+                    dp[left][right] = Math.max(dp[left][right], dp[left][k - 1] + dp[k + 1][right] + arr[left - 1] * arr[k] * arr[right + 1]);
+                }
             }
         }
-        return 0;
-
+        return dp[1][nums.length];
     }
 }
