6d9a7a445ab9d516fd434e8432b1cc8ed2992732
Jun Zhou <jun@Juns-MacBook-Pro.local>
2016-03-31 20:59:29 -0700

    Chameleon!

---
+++ src/P75.java
@@ -0,0 +1,59 @@
+/**
+ * Created by jun on 3/31/16.
+ * 75. Sort Colors
+ */
+public class P75 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 2, 2, 2, 0, 2, 1, 1};
+        P75 p = new P75();
+        p.sortColors(arr);
+
+        for (int a : arr)
+            System.out.println(a);
+    }
+
+    public void sortColors(int[] nums) {
+        int left =0;
+        int right = nums.length-1;
+
+        for (int i =0; i<=right; i++){
+            if (nums[i] == 0)
+                swap(nums, i, left++);
+
+            if (nums[i]==2)
+                swap(nums, i--, right--);
+        }
+    }
+
+// This is my own way, O(N), but dirty.
+//    public void sortColors(int[] nums) {
+//        int len = nums.length;
+//        if (len == 0)
+//            return;
+//
+//        int left = 0;
+//        while (left < len && nums[left] == 0) left++;
+//        int right = len - 1;
+//        while (right >= 0 && nums[right] == 2) right--;
+//
+//        for (int i = left; left < right && i <= right; i++) {
+//            if (nums[i] == 2) {
+//                swap(nums, right, i);
+//                while (right >= 0 && nums[right] == 2) right--;
+//            }
+//
+//            if (nums[i] == 0) {
+//                swap(nums, left, i);
+//                left++;
+//            }
+//
+//        }
+//    }
+
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
