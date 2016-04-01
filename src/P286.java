33432f705e778796b028754560dac16e1f8e5c56
Jun Zhou <jun@Juns-MacBook-Pro.local>
2016-03-31 19:53:12 -0700

    Clean up Walls and Gates a bit.

--- src/P286.java
+++ src/P286.java
@@ -20,32 +20,24 @@ public class P286 {
                     queue.offer(new Location(i, j));
 
         while (!queue.isEmpty()) {
-            Location l = queue.poll();
-            expand(rooms, l.x, l.y, queue, r, c);
+            expand(rooms, queue.poll(), queue, r, c);
         }
-
     }
 
-    public void expand(int[][] rooms, int i, int j, Queue<Location> queue, int r, int c) {
-        int curr = rooms[i][j];
+    public void expand(int[][] rooms, Location l, Queue<Location> queue, int r, int c) {
+        int x = l.x;
+        int y = l.y;
+        int curr = rooms[x][y];
+        int[] deltaX = new int[]{-1, 0, 1, 0};
+        int[] deltaY = new int[]{0, -1, 0, 1};
 
-        if (i - 1 >= 0 && rooms[i - 1][j] == Integer.MAX_VALUE) {
-            rooms[i - 1][j] = curr + 1;
-            queue.offer(new Location(i - 1, j));
-        }
-
-        if (i + 1 < r && rooms[i + 1][j] == Integer.MAX_VALUE) {
-            rooms[i + 1][j] = curr + 1;
-            queue.offer(new Location(i + 1, j));
-        }
-        if (j - 1 >= 0 && rooms[i][j - 1] == Integer.MAX_VALUE) {
-            rooms[i][j - 1] = curr + 1;
-            queue.offer(new Location(i, j - 1));
-        }
-
-        if (j + 1 < c && rooms[i][j + 1] == Integer.MAX_VALUE) {
-            rooms[i][j + 1] = curr + 1;
-            queue.offer(new Location(i, j + 1));
+        for (int i = 0; i < deltaX.length; i++) {
+            int nextX = x + deltaX[i];
+            int nextY = y + deltaY[i];
+            if (nextX >= 0 && nextX < r && nextY >= 0 && nextY < c && rooms[nextX][nextY] == Integer.MAX_VALUE){
+                rooms[nextX][nextY] = curr + 1;
+                queue.offer(new Location(nextX, nextY));
+            }
         }
     }
 
