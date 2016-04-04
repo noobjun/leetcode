cf1e59682cc4ed9a0fd76a03f3921de26fc1d07e
Jun Zhou <jun@Juns-MacBook-Pro.local>
2016-04-03 18:12:38 -0700

    Decode ways.

--- src/P91.java
+++ src/P91.java
@@ -1,46 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jun on 3/5/16.
  * 91. Decode Ways
+ * <p>
+ * Wow spent so much on this, nasty edge cases. Next time I should do this in dp.
  */
 public class P91 {
-
     public int numDecodings(String s) {
         if (s.isEmpty())
             return 0;
 
-        for (int i = 0; i < s.length(); i++)
-            if (i == 0 && s.charAt(i) == '0' || s.charAt(i) == '0' && s.charAt(i - 1) != '1' && s.charAt(i - 1) != '2')
-                return 0;
-
-        int[] dp = new int[s.length()];
-        for (int i = 0; i < dp.length; i++)
-            dp[i] = -1;
-
-        return helper(s, 0, dp);
+        Map<String, Integer> map = new HashMap<>();
+        return helper(s, map);
     }
 
-    public int helper(String s, int index, int[] dp) {
-        int len = s.length();
-        if (index >= len - 1)
-            return 1;
-
-        if (dp[index] != -1)
-            return dp[index];
+    public int helper(String s, Map<String, Integer> map) {
+        if (map.containsKey(s))
+            return map.get(s);
 
         int result;
-        char current = s.charAt(index);
-        char next = s.charAt(index + 1);
+        int len = s.length();
 
-        if (next == '0') {
-            result = helper(s, index + 2, dp);
+        if (len > 0 && s.charAt(0) == '0') {
+            result = 0;
         } else {
-            if (Integer.valueOf("" + current + next) <= 26)
-                result = helper(s, index + 1, dp) + helper(s, index + 2, dp);
-            else
-                result = helper(s, index + 1, dp);
-        }
+            if (len <= 1) {
+                result = 1;
+            } else {
+                int first = s.charAt(0) - '0';
+                int second = s.charAt(1) - '0';
+                int num = first * 10 + second;
 
-        dp[index] = result;
+                if (num > 26) {
+                    result = helper(s.substring(1), map);
+                } else {
+                    result = helper(s.substring(1), map) + helper(s.substring(2), map);
+                }
+            }
+        }
+        map.put(s, result);
         return result;
     }
 }
