67e428874e7d2e219e756bbeebeeedff0c09459b
Jun Zhou <hjnouuz@gmail.com>
2016-04-25 11:06:19 -0700

    Added comments on how to be smart about inserting interval.

--- src/P57.java
+++ src/P57.java
@@ -4,6 +4,10 @@ import java.util.List;
 /**
  * Created by jun on 4/25/16.
  * 57. Insert Interval
+ * <p>
+ * 3 cases, if oldInterval.end < newInterval.start, add oldInterval to result, and index++
+ *          if oldInterval.start > newInterval.end, simply add oldInterval, no effect on index
+ *          else we merge the oldInterval with the newInterval and assign it to newInterval
  */
 public class P57 {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
