e747a0a6e21def16c8eb939a9245cdd65b4ffcd9
Jun Zhou <hjnouuz@gmail.com>
2016-04-30 19:19:39 -0700

    Generate power sets.

--- src/P218.java
+++ src/P218.java
@@ -1,9 +1,12 @@
+import java.util.List;
+
 /**
  * Created by jun on 4/29/16.
  * 218. The Skyline Problem
  */
 public class P218 {
     public List<int[]> getSkyline(int[][] buildings) {
+        return null;
 
     }
 }
--- src/P90.java
+++ src/P90.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -6,6 +8,25 @@ import java.util.List;
  */
 public class P90 {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
 
+        for (int i = 0; i < nums.length; i++) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) i++;
+            int count = i - start + 1;
+
+            int resultSize = result.size();
+            for (int j = 0; j < resultSize; j++) {
+                List<Integer> partial = new ArrayList<>(result.get(j));
+                for (int k = 0; k < count; k++) {
+                    partial.add(nums[i]);
+                    result.add(new ArrayList<>(partial)); // this line is the magic
+                }
+            }
+        }
+
+        return result;
     }
 }
