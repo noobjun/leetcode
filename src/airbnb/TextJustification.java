22f6efae6b19584935265bf0f6aefd2258d9223c
Jun Zhou <hjnouuz@gmail.com>
2016-05-24 22:45:04 -0700

    justified again

--- src/airbnb/TextJustification.java
+++ src/airbnb/TextJustification.java
@@ -10,25 +10,26 @@ import java.util.List;
 public class TextJustification {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> result = new LinkedList<>();
-
-        if (words.length == 0) {
+        if (words.length == 0)
             return result;
-        }
 
         ArrayList<String> tokens = new ArrayList<>();
         int rawLen = 0;
 
         for (String word : words) {
-            if (rawLen + tokens.size() + word.length() > maxWidth) {
-                result.add(generateLine(tokens, rawLen, maxWidth));
+            int wordLen = word.length();
+            if (tokens.size() + rawLen + wordLen > maxWidth) {
+                // print out this line
+                result.add(format(tokens, maxWidth, rawLen));
                 tokens.clear();
                 rawLen = 0;
             }
 
-            rawLen += word.length();
             tokens.add(word);
+            rawLen += wordLen;
         }
 
+        // last line
         String lastLine = "";
         for (int i = 0; i < tokens.size(); i++) {
             if (i == tokens.size() - 1) {
@@ -37,49 +38,116 @@ public class TextJustification {
                 lastLine += tokens.get(i) + " ";
             }
         }
-
-        for (int i = lastLine.length(); i < maxWidth; i++) {
+        for (int i = lastLine.length(); i < maxWidth; i++)
             lastLine += " ";
-        }
 
-        tokens.clear();
         result.add(lastLine);
-
         return result;
     }
 
-
-    public String generateLine(ArrayList<String> tokens, int rawLen, int maxWidth) {
+    public String format(List<String> tokens, int maxWidth, int rawLen) {
         String result = "";
+
         if (tokens.size() == 1) {
             result += tokens.get(0);
             for (int i = result.length(); i < maxWidth; i++)
                 result += " ";
+        } else {
+            int spaces = maxWidth - rawLen;
+            int spacePerWord = spaces / (tokens.size() - 1);
+            int remain = spaces % (tokens.size() - 1);
 
-            return result;
-        }
-
-        int spaces = maxWidth - rawLen;
-        int spacesPerWord = spaces / (tokens.size() - 1);
-        int remain = spaces % (tokens.size() - 1);
-
-        for (int i = 0; i < tokens.size(); i++) {
-            if (i == tokens.size() - 1)
-                result += tokens.get(i);
-            else {
-                result += tokens.get(i);
-                for (int j = 0; j < spacesPerWord; j++)
-                    result += " ";
-
-                if (remain > 0) {
-                    result += " ";
-                    remain--;
+            for (int i = 0; i < tokens.size(); i++) {
+                if (i == tokens.size() - 1) {
+                    result += tokens.get(i);
+                } else {
+                    result += tokens.get(i);
+                    for (int j = 0; j < spacePerWord; j++) {
+                        result += " ";
+                    }
+                    if (remain > 0) {
+                        result += " ";
+                        remain--;
+                    }
                 }
             }
         }
-
         return result;
     }
+
+
+//    public List<String> fullJustify(String[] words, int maxWidth) {
+//        List<String> result = new LinkedList<>();
+//
+//        if (words.length == 0) {
+//            return result;
+//        }
+//
+//        ArrayList<String> tokens = new ArrayList<>();
+//        int rawLen = 0;
+//
+//        for (String word : words) {
+//            if (rawLen + tokens.size() + word.length() > maxWidth) {
+//                result.add(generateLine(tokens, rawLen, maxWidth));
+//                tokens.clear();
+//                rawLen = 0;
+//            }
+//
+//            rawLen += word.length();
+//            tokens.add(word);
+//        }
+//
+//        String lastLine = "";
+//        for (int i = 0; i < tokens.size(); i++) {
+//            if (i == tokens.size() - 1) {
+//                lastLine += tokens.get(i);
+//            } else {
+//                lastLine += tokens.get(i) + " ";
+//            }
+//        }
+//
+//        for (int i = lastLine.length(); i < maxWidth; i++) {
+//            lastLine += " ";
+//        }
+//
+//        tokens.clear();
+//        result.add(lastLine);
+//
+//        return result;
+//    }
+//
+//
+//    public String generateLine(ArrayList<String> tokens, int rawLen, int maxWidth) {
+//        String result = "";
+//        if (tokens.size() == 1) {
+//            result += tokens.get(0);
+//            for (int i = result.length(); i < maxWidth; i++)
+//                result += " ";
+//
+//            return result;
+//        }
+//
+//        int spaces = maxWidth - rawLen;
+//        int spacesPerWord = spaces / (tokens.size() - 1);
+//        int remain = spaces % (tokens.size() - 1);
+//
+//        for (int i = 0; i < tokens.size(); i++) {
+//            if (i == tokens.size() - 1)
+//                result += tokens.get(i);
+//            else {
+//                result += tokens.get(i);
+//                for (int j = 0; j < spacesPerWord; j++)
+//                    result += " ";
+//
+//                if (remain > 0) {
+//                    result += " ";
+//                    remain--;
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
 
 
 //    public List<String> fullJustify(String[] words, int maxWidth) {
